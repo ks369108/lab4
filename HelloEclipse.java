e5cf35199f6d4a7a0fc8787c1ac4c53b66547335
foo <ks369108@gmail.com>
2021-05-16 14:01:37 +0800

    first version

---
+++ HelloEclipse.java
@@ -0,0 +1,30 @@
+package xxx;
+
+public class HelloEclipse {
+	
+	//單行註解
+	
+	/*
+	 *多行註解
+	 */
+	
+	/**********
+	 *        *
+	 *        * 
+	 *        *
+	 *        *
+	 **********/
+	
+	public static void main(String[] args) {
+		
+		int i1 = 100;
+		System.out.println(i1);
+		
+		System.out.println("Hello!");
+//		System.out.println("cyrl+/ is comment");
+	}
+	
+
+
+	
+}
